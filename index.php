a42c32d2af607a1323561e3962278627310def79
Tova Folkesson <tova.folkesson@gmail.com>
2026-02-07 23:10:08 +0100

    starting point

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="images/alipackicon.png">
+        <title>AliPack</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+<body>
+    <div class="container"> <!-- This class is for the entire body content -->
+        <header class="site-header">
+            <div class="header-icon">
+                <a href="index.php"><img src="images/alipackicon.png" alt="Logotype"></a>
+            </div>
+            <div class="header-title">
+                <img src="images/alipackiconheader.png" alt="Logotype">
+            </div>
+            <div class="header-navigation">
+                <nav>
+                    <a href="products.php">Products</a>
+                    <a href="contact.php">Contact us</a>
+                </nav>
+            </div>
+        </header>
+
+        <h1>test</h1>
+
+        <footer>
+            <div class="site-footer">
+                <div class="footer-contacts">
+                    <ul>
+                        <li>Via G. Puccini 81, Oste Montemurlo, 59103 Prato</li>
+                        <li><a href="callto:+393894687801">+39 389 468 7801</a></li>
+                        <li>E-mail:<a href="mailto:alipackitalia@gmail.com">alipackitalia@gmail.com</a></li>
+                    </ul>
+                </div>
+                <div class="footer-menu">
+                    <nav>
+                        <a href="products.php">Products</a>
+                        <a href="contact.php">Contact us</a>
+                    </nav>
+                </div>
+                <div class="footer-rights">
+                    <p>© 2026 AliPackItalia. All Rights Reserved.</p>
+                    <!-- add privacy and terms pages! -->
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Terms of Service</a>
+                </div>
+            </div>
+        </footer>
+    </div> <!-- closes <div class="container"> from body -->
+</body>
+</html>
---
+++ prep for wordpress/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="site-footer">
+        <div class="footer-menu">
+            <!-- add navigation here -->
+        </div>
+        <div class="footer-rights">
+            <p>© 2026 AliPackItalia. All Rights Reserved.</p>
+            <!-- add privacy and terms pages! -->
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Service</a>
+        </div>
+    </div> <!-- closes <div class="container"> from body -->
+</footer>
+</body>
+</html>
