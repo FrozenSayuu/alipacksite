4f9b7ba370c0fc776283df2bf1e308f71158ef1e
Tova Folkesson <tova.folkesson@gmail.com>
2026-02-13 14:51:22 +0100

    added contact form plugin and basic styling

---
+++ wp-content/themes/alipacksite/page.php
@@ -0,0 +1,35 @@
+<?php get_header(); 
+
+?>
+
+<div class="page">
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) : the_post(); ?>
+			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+				<h2><?php the_title(); ?></h2>
+				
+				<?php
+				if( the_post_thumbnail() ) : ?>
+					<div id="our-post-image">
+						<?php the_post_thumbnail('alipacksite-gallery'); ?>
+					</div>
+				<?php
+				endif;
+				the_content(); ?>
+			</article>
+		<?php
+		endwhile;
+
+		if ( is_single() ) :
+			previous_post_link();
+			next_post_link();
+		endif;
+	else :
+		_e( 'Oj hoppsan, här var det tomt.', 'textdomain' );
+	endif;
+	?>
+
+</div>
+
+<?php get_footer(); ?>
