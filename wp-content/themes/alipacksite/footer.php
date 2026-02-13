c78c60d77c9fd747088c775e64bed78aa0099de4
Tova Folkesson <tova.folkesson@gmail.com>
2026-02-13 13:02:36 +0100

    added basic css to footer

--- wp-content/themes/alipacksite/footer.php
+++ wp-content/themes/alipacksite/footer.php
@@ -1,23 +1,31 @@
         <footer>
             <div class="site-footer">
-                <div class="footer-menu">
-                    <?php wp_nav_menu(['theme_location' => 'footer', 'container' => 'nav']); ?>
-                </div>
-                
                 <div class="footer-contacts">
                     <ul>
                         <li>Via G. Puccini 81, Oste Montemurlo, 59103 Prato</li>
-                        <li><a href="callto:+393894687801">+39 389 468 7801</a></li>
-                        <li>E-mail:<a href="mailto:alipackitalia@gmail.com">alipackitalia@gmail.com</a></li>
+                        <li>Phone: <a href="callto:+393894687801">+39 389 468 7801</a></li>
+                        <li>E-mail: <a href="mailto:alipackitalia@gmail.com">alipackitalia@gmail.com</a></li>
+                        <li><p id="rights-text">© 2026 <?php bloginfo( 'name' ) ?>. All Rights Reserved.</p></li>
                     </ul>
                 </div>
-                
+
+                <div class="footer-menu">
+                    <h3>Pages</h3>
+                    <?php wp_nav_menu(['theme_location' => 'footer', 'container' => 'nav']); ?>
+                </div>
+                <div class="footer-secmenu">
+                    <h3>Faq</h3>
+                    <ul>
+                        <li><a href="../privacy-policy/">Privacy Policy</a></li>
+                        <li><a href="../terms-of-service/">Terms of Service</a></li>
+                        <li><a href="../cookie-policy/">Cookie Policy</a></li>
+                    </ul>
+                </div>
+                <!-- in case of later use
                 <div class="footer-rights">
-                    <p>© 2026 <?php bloginfo( 'name' ) ?>. All Rights Reserved.</p>
-                    <!-- add privacy and terms pages! -->
                     <a href="../privacy-policy/">Privacy Policy</a>
                     <a href="../terms-of-service/">Terms of Service</a>
-                </div>
+                </div> -->
             </div>
         </footer>
     </div> <!-- closes <div class="container"> from body -->
